1802069b7bda07424c51187e64068cd5da6090b8
Will Vining <wfvining@gmail.com>
2020-01-08 10:46:23 -0700

    Implemented `put` and `update` on MDArray
    Allows values to be stored/set, and allows stored values to be updated
    by a function which takes two parameters: a new/default value and the
    current value.

--- lib/mdarray.ex
+++ lib/mdarray.ex
@@ -15,26 +15,6 @@ defmodule MDArray do
     }
   end
 
-  @doc """
-  Gets an element from the array.
-  """
-  def get(%{dimensions: dimensions}, index) when length(index) != length(dimensions) do
-    raise ArgumentError, "dimension mismatch"
-  end
-
-  def get(array, index) do
-    check_index(array.dimensions, index)
-    List.foldl(
-      index,
-      array.data,
-      fn
-        _, :undefined ->
-          :undefined
-        i, acc ->
-          :array.get(i, acc)
-      end)
-  end
-
   defp check_index([], []), do: :ok
 
   defp check_index(_, []), do: raise ArgumentError, "dimension mismatch"
@@ -49,4 +29,58 @@ defmodule MDArray do
     end
   end
 
+  @doc """
+  Gets an element from the array.
+  """
+  def get(array, index) do
+    check_index(array.dimensions, index)
+    List.foldl(
+      index,
+      array.data,
+      fn
+        _, :undefined ->
+          :undefined
+        i, acc ->
+          :array.get(i, acc)
+      end)
+  end
+
+  @doc """
+  Insert and element into the array.
+  """
+  def put(array, index, value) do
+    check_index(array.dimensions, index)
+    %{ array | data: put_internal(array.data, index, value) }
+  end
+
+  defp put_internal(:undefined, index, value) do
+    put_internal(:array.new(), index, value)
+  end
+
+  defp put_internal(array, [ix], value) do
+    :array.set(ix, value, array)
+  end
+
+  defp put_internal(array, [ix|index], value) do
+    :array.set(
+      ix,
+      put_internal(:array.get(ix, array), index, value),
+      array
+    )
+  end
+
+  @doc """
+  Update the value stored at `index` by applying `update_fun` if it is
+  defined, otherwise store the default.
+
+  `update_fun` takes two parameters, the current stored value and `default`.
+  """
+  def update(array, index, default, update_fun) do
+    case get(array, index) do
+      :undefined ->
+        put(array, index, default)
+      current ->
+        put(array, index, update_fun.(current, default))
+    end
+  end
 end
--- test/mdarray_test.exs
+++ test/mdarray_test.exs
@@ -4,6 +4,7 @@ defmodule MDArrayTest do
 
   setup do
     %{ array1: MDArray.new([10]),
+       array3: MDArray.new([3,3,3]),
        array5: MDArray.new([5, 5, 5, 5, 5]) }
   end
 
@@ -38,5 +39,64 @@ defmodule MDArrayTest do
     assert_raise ArgumentError, "dimension mismatch", fn ->
       MDArray.get(context.array5, [1, 2, 3, 4, 0, 1])
     end
+  end
+
+  test "put dimension mismatch raises an error", context do
+    assert_raise ArgumentError, "dimension mismatch", fn ->
+      MDArray.put(context.array5, [0, 1, 0, 1], :test)
+    end
+
+    assert_raise ArgumentError, "dimension mismatch", fn ->
+      MDArray.put(context.array5, [1, 1, 1, 1, 0, 0], :test)
+    end
+  end
+
+  test "put index out of range raises an error", context do
+    assert_raise ArgumentError, "index out of bounds", fn ->
+      MDArray.put(context.array5, [1, 2, 3, 4, 5], :test)
+    end
+
+    assert_raise ArgumentError, "index out of bounds", fn ->
+      MDArray.put(context.array1, [-1], :test)
+    end
+  end
+
+  test "can add entries to the array", context do
+    arr = context.array1
+    |> MDArray.put([0], 0)
+    |> MDArray.put([1], 1)
+    |> MDArray.put([2], 2)
+
+    assert arr != context.array1
+  end
+
+  test "can retrieve value after put", context do
+    a = MDArray.put(context.array5, [0, 1, 2, 3, 4], :test)
+    assert MDArray.get(a, [0, 1, 2, 3, 4]) == :test
+  end
+
+  test "only the correct value is set by put", context do
+    a = MDArray.put(context.array3, [1, 1, 1], :test)
+
+    for i <- 0..2, j <- 0..2, k <- 0..2, i != 1 or j != 1 or k != 1 do
+      assert MDArray.get(a, [i,j,k]) == :undefined
+    end
+  end
+
+  test "set a value twice results in second value", context do
+    a = MDArray.put(MDArray.put(context.array1, [0], :test1), [0], :test2)
+    assert MDArray.get(a, [0]) == :test2
+  end
+
+  test "update on undefined value set default", context do
+    a = MDArray.update(context.array1, [0], :default, fn _, _ -> :nondefault end)
+
+    assert MDArray.get(a, [0]) == :default
+  end
+
+  test "update on defined value applies the update function", context do
+    a = context.array1 |> MDArray.put([1], 1) |> MDArray.update([1], :default, fn x, _ -> x*2 end)
+
+    assert MDArray.get(a, [1]) == 2
   end
 end
